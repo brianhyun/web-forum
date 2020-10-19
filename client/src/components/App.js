d283af62a29f7a0bb9cff241de2481cc1a74e8fe
Brian Hyun <jso.venn@gmail.com>
2020-10-19 05:53:39 -0700

    Import necessary redux modules and add redux boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './ui/theme';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+
 // Components
 import Landing from './main/Landing';
 import Login from './auth/Login';
@@ -14,16 +18,18 @@ import Signup from './auth/Signup';
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
+        <Provider store={store}>
             <Router>
-                <div className="App">
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                </div>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="App">
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/signup" component={Signup} />
+                    </div>
+                </ThemeProvider>
             </Router>
-        </ThemeProvider>
+        </Provider>
     );
 }
 
