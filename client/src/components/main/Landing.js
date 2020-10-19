19a00bc2a6a8584cb3e9b337a6114c65bfb7c24a
Brian Hyun <jso.venn@gmail.com>
2020-10-19 05:16:58 -0700

    Fix styles for landing, login, and signup page

--- client/src/components/main/Landing.js
+++ client/src/components/main/Landing.js
@@ -6,85 +6,102 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import GroupWorkOutlinedIcon from '@material-ui/icons/GroupWorkOutlined';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundColor: '#1E3B70',
+        padding: theme.spacing(0, 10),
+    },
     box: {
-        background: 'linear-gradient(to bottom right, #29539B, #1E3B70)',
         display: 'flex',
         flexDirection: 'column',
         height: '100vh',
         justifyContent: 'center',
-        padding: theme.spacing(0, 10),
+        alignItems: 'flex-start',
     },
     header: {
         color: '#ffffff',
-        fontWeight: 500,
-        marginBottom: theme.spacing(4),
+        marginBottom: theme.spacing(3),
     },
     subtitle: {
         color: '#ffffff',
-        fontFamily: 'Roboto',
-        fontSize: 28,
-        marginBottom: theme.spacing(6),
+        fontSize: 20,
+        marginBottom: theme.spacing(5),
     },
     login: {
         backgroundColor: '#ffffff',
-        marginRight: theme.spacing(6),
-        padding: theme.spacing(1.5, 8),
+        color: theme.palette.primary.main,
+        padding: theme.spacing(1, 0),
     },
     signup: {
-        color: '#ffffff',
         borderColor: '#ffffff',
-        padding: theme.spacing(1.5, 8),
+        color: '#ffffff',
+        padding: theme.spacing(1, 0),
     },
 }));
 
 const linkStyle = {
     textDecoration: 'none',
+    width: '100%',
 };
 
 function Landing() {
     const classes = useStyles();
 
     return (
-        <Box>
-            <Box className={classes.box}>
-                <Typography
-                    variant="h2"
-                    component="h1"
-                    className={classes.header}
-                >
-                    The Forum
-                </Typography>
-                <Typography
-                    variant="h4"
-                    component="h2"
-                    className={classes.subtitle}
-                >
-                    A structured approach to the chaotic group chat.
-                </Typography>
-                <Box>
-                    <Link to="/login" style={linkStyle}>
-                        <Button
-                            variant="contained"
-                            size="large"
-                            className={classes.login}
+        <Container component="main" maxWidth="xl" className={classes.container}>
+            <CssBaseline />
+            <Grid container>
+                <Grid item xs={12} sm={6}>
+                    <Box className={classes.box}>
+                        <Typography
+                            variant="h4"
+                            component="h1"
+                            className={classes.header}
                         >
-                            Log In
-                        </Button>
-                    </Link>
-                    <Link to="/signup" style={linkStyle}>
-                        <Button
-                            variant="outlined"
-                            size="large"
-                            className={classes.signup}
+                            The Forum
+                        </Typography>
+                        <Typography
+                            variant="body1"
+                            className={classes.subtitle}
                         >
-                            Sign Up
-                        </Button>
-                    </Link>
-                </Box>
-            </Box>
-        </Box>
+                            A structured approach to the chaotic group chat.
+                        </Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <Link to="/login" style={linkStyle}>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        className={classes.login}
+                                    >
+                                        Log In
+                                    </Button>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <Link to="/signup" style={linkStyle}>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="outlined"
+                                        className={classes.signup}
+                                    >
+                                        Sign Up
+                                    </Button>
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
 
