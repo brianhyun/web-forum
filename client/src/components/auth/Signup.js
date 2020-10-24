04a83ae777dc8aab64937d2ca7989bc856144ac5
Brian Hyun <jso.venn@gmail.com>
2020-10-24 12:36:41 -0700

    Remove copyright labels and avator icon from signup and login labels

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -9,17 +9,13 @@ import { signupUser } from '../../redux/slices/authSlice';
 import { selectFormErrors } from '../../redux/slices/errorsSlice';
 
 // Material UI Styles
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Copyright from '../tags/Copyright';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -28,10 +24,6 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         justifyContent: 'center',
         height: '100vh',
-    },
-    avatar: {
-        margin: theme.spacing(3),
-        backgroundColor: theme.palette.secondary.main,
     },
     form: {
         width: '100%',
@@ -93,9 +85,6 @@ function Signup(props) {
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
@@ -197,9 +186,6 @@ function Signup(props) {
                         </Grid>
                     </Grid>
                 </form>
-                <Box mt={5}>
-                    <Copyright />
-                </Box>
             </div>
         </Container>
     );
