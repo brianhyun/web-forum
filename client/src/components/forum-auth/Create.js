b3069d187fceffad933459ffb43ee4151f427c96
Brian Hyun <jso.venn@gmail.com>
2020-10-27 13:07:28 -0700

    Create public and private options on create forum page

---
+++ client/src/components/forum-auth/Create.js
@@ -0,0 +1,214 @@
+// Dependencies
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// Redux
+import { useDispatch } from 'react-redux';
+
+// Material UI Styles
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: '#1E3B70',
+    },
+    box: {
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+    },
+    paper: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: theme.spacing(4, 3),
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(2),
+    },
+    toggle: {
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: theme.spacing(2),
+    },
+    submit: {
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2, 0),
+    },
+    margin: {
+        marginBottom: theme.spacing(2),
+    },
+    gridItem: {
+        paddingBottom: 0,
+    },
+}));
+
+// Custom React Styles
+const linkStyle = {
+    textDecoration: 'none',
+};
+
+function Join() {
+    // Use Material UI Styles
+    const classes = useStyles();
+
+    // Redux Handles
+    const dispatch = useDispatch();
+
+    // React Handles
+    const [forum, setForum] = useState({
+        name: '',
+        password: '',
+        errors: {},
+    });
+
+    function handleChange(event) {
+        setForum({
+            ...forum,
+            [event.target.id]: event.target.value,
+        });
+    }
+
+    function handleSubmit(event) {
+        // Prevent Default Form Behavior
+        event.preventDefault();
+
+        // Create Forum Object
+        const forumObj = {
+            name: forum.name,
+            password: forum.password,
+        };
+
+        // Dispatch Login Forum Action
+        dispatch();
+    }
+
+    const [isPublic, setIsPublic] = useState(true);
+
+    function clickPublic() {
+        if (!isPublic) {
+            setIsPublic(true);
+        }
+    }
+
+    function clickPrivate() {
+        if (isPublic) {
+            setIsPublic(false);
+        }
+    }
+
+    return (
+        <Box component="main" className={classes.root}>
+            <Container maxWidth="xs">
+                <CssBaseline />
+                <Box className={classes.box}>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Typography component="h1" variant="h5">
+                            Create a Forum
+                        </Typography>
+                        <form
+                            className={classes.form}
+                            noValidate
+                            onSubmit={handleSubmit}
+                        >
+                            <TextField
+                                className={classes.margin}
+                                variant="outlined"
+                                name="name"
+                                required
+                                fullWidth
+                                label="Forum Name"
+                                id="name"
+                                autoFocus
+                                onChange={handleChange}
+                                value={forum.name}
+                            />
+                            {!isPublic && (
+                                <TextField
+                                    className={classes.margin}
+                                    variant="outlined"
+                                    name="password"
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    id="password"
+                                    type="password"
+                                    onChange={handleChange}
+                                    value={forum.password}
+                                />
+                            )}
+                            <Grid
+                                container
+                                spacing={2}
+                                className={classes.toggle}
+                            >
+                                <Grid item xs={6}>
+                                    <Button
+                                        fullWidth
+                                        onClick={clickPublic}
+                                        variant={
+                                            isPublic ? 'contained' : 'outlined'
+                                        }
+                                        color={
+                                            isPublic ? 'secondary' : 'default'
+                                        }
+                                    >
+                                        Public
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Button
+                                        fullWidth
+                                        onClick={clickPrivate}
+                                        variant={
+                                            !isPublic ? 'contained' : 'outlined'
+                                        }
+                                        color={
+                                            !isPublic ? 'secondary' : 'default'
+                                        }
+                                    >
+                                        Private
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justify="center">
+                                <Grid item>
+                                    <Link to="/join" style={linkStyle}>
+                                        <Typography
+                                            variant="body2"
+                                            color="primary"
+                                        >
+                                            Join a Forum
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </Paper>
+                </Box>
+            </Container>
+        </Box>
+    );
+}
+
+export default Join;
--- routes/api/forums.js
+++ routes/api/forums.js
@@ -9,7 +9,44 @@ router.post('/api/forums/join', (req, res, next) => {
     // Retrieve Input
     const input = req.body;
 
+    // Expand Inputs
+    const forumName = input.name;
+
     // Check Database for Forum
+    Forum.findOne({ name: forumName })
+        .then((forum) => {
+            if (forum) {
+                // Check User-Submitted Password to Stored Password
+            } else {
+                // Forum Doesn't Exist
+                return res.status(400).json({
+                    forum: 'A forum with this name cannot be found.',
+                });
+            }
+        })
+        .catch((err) => console.error(err));
+});
+
+router.post('/api/forums/create', (req, res, next) => {
+    // Retrieve Input
+    const input = req.body;
+
+    // Expand Inputs
+    const forumName = input.name;
+
+    // Check Database for Forum
+    Forum.findOne({ name: forumName })
+        .then((forum) => {
+            if (forum) {
+                // Forum Doesn't Exist
+                return res.status(400).json({
+                    forum: 'This name is already taken.',
+                });
+            } else {
+                // Create New Forum
+            }
+        })
+        .catch((err) => console.error(err));
 });
 
 module.exports = router;
