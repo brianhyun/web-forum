5e9b7e20e27bc02105c435141ae07f49dba45374
Brian Hyun <jso.venn@gmail.com>
2020-11-11 00:04:42 -0800

    Fix styles for dashboard page

--- client/src/components/main/Dashboard.js
+++ client/src/components/main/Dashboard.js
@@ -14,67 +14,103 @@ import { getForumInfo } from '../../redux/slices/forumSlice';
 import { selectCurrentForum } from '../../redux/slices/forumSlice';
 
 // Material UI Styles
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@material-ui/core/IconButton';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import MenuIcon from '@material-ui/icons/Menu';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 // Custom Styles
 import './Dashboard.css';
 
-const drawerWidth = 65;
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
     },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
+        whiteSpace: 'nowrap',
     },
-    drawerPaper: {
+    drawerOpen: {
         width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
     },
-    listItem: {
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
     },
     content: {
         flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3),
-    },
-    headingContainer: {
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginBottom: theme.spacing(3),
     },
     paper: {
         padding: theme.spacing(3),
-    },
-    postButtonContainer: {
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        marginTop: theme.spacing(3),
     },
 }));
 
 function Dashboard(props) {
     // Use Material UI Styles
     const classes = useStyles();
+    const theme = useTheme();
 
     // Redux Handles
     const dispatch = useDispatch();
@@ -121,73 +157,107 @@ function Dashboard(props) {
         console.log(postContent);
     }
 
+    // Open and Close Navigation Drawer
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Box className={classes.root}>
             <CssBaseline />
 
-            {/* Navigation Menu */}
-            <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-                anchor="left"
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
             >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open,
+                        })}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        {currentForum.name}
+                    </Typography>
+                    <Box>
+                        <IconButton
+                            disableRipple
+                            disableFocusRipple
+                            size="small"
+                            onClick={handleClick}
+                        >
+                            <Avatar alt="forum profile picture" src="" />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+            >
+                <div className={classes.toolbar}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? (
+                            <ChevronRightIcon />
+                        ) : (
+                            <ChevronLeftIcon />
+                        )}
+                    </IconButton>
+                </div>
+                <Divider />
                 <List>
-                    {/* All Forums */}
                     {usersForums.map((forum) => {
                         const forumLink = `/forum/${forum.id}`;
                         const forumName = forum.name;
 
                         return (
-                            <ListItem
-                                key={forum.id}
-                                className={classes.listItem}
-                            >
-                                <IconButton
-                                    disableRipple
-                                    disableFocusRipple
-                                    size="small"
-                                >
+                            <ListItem button key={forum.id}>
+                                <ListItemAvatar>
                                     <Link to={forumLink}>
                                         <Avatar alt={forumName} src="" />
                                     </Link>
-                                </IconButton>
+                                </ListItemAvatar>
+                                <ListItemText primary={forumName} />
                             </ListItem>
                         );
                     })}
-
-                    {/* Join/Create List Item */}
-                    <ListItem className={classes.listItem}>
-                        <IconButton
-                            disableRipple
-                            disableFocusRipple
-                            size="small"
-                        >
-                            <Link to="/join">
-                                <Avatar>
-                                    <AddIcon />
-                                </Avatar>
-                            </Link>
-                        </IconButton>
+                    <Divider />
+                    <ListItem button className={classes.listItem}>
+                        <Link to="/join">
+                            <Avatar>
+                                <AddIcon />
+                            </Avatar>
+                        </Link>
                     </ListItem>
                 </List>
             </Drawer>
 
             {/* Main Content Section */}
             <Box component="main" className={classes.content}>
-                <Box className={classes.headingContainer}>
-                    <Typography>{currentForum.name}</Typography>
-                    <IconButton
-                        disableRipple
-                        disableFocusRipple
-                        size="small"
-                        onClick={handleClick}
-                    >
-                        <Avatar alt="forum profile picture" src="" />
-                    </IconButton>
-                </Box>
+                <div className={classes.toolbar} />
 
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={8}>
