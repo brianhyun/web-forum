99c43cc8ef72d8f710f6a366c5219d3b35633faf
Brian Hyun <jso.venn@gmail.com>
2020-12-19 17:55:34 -0800

    Clean up passport configuration

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -4,25 +4,29 @@ const User = require(rootPath + '/models/User');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-const opts = {
+const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_PRIVATE_KEY,
 };
 
-function passportConfig(passport) {
-    passport.use(
-        new JwtStrategy(opts, (jwt_payload, done) => {
-            User.findById(jwt_payload.id)
-                .then((user) => {
-                    if (user) {
-                        return done(null, user);
-                    }
+function verify(jwt_payload, done) {
+    User.findById(jwt_payload.id)
+        .then((user) => {
+            if (err) {
+                return done(err, false);
+            }
 
-                    return done(null, false);
-                })
-                .catch((err) => done(err, false));
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
         })
-    );
+        .catch((err) => console.error(err));
+}
+
+function passportConfig(passport) {
+    passport.use(new JwtStrategy(options, verify));
 }
 
 module.exports = passportConfig;
